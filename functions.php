6c2957e225c61b8ebd54b0b2319e6a448f00aa8c
MD. Shohel Rana <shohel065931@gmail.com>
2025-05-02 12:18:56 +0600

    gjh

--- functions.php
+++ functions.php
@@ -30,17 +30,109 @@ add_action('wp_enqueue_scripts', 'sample_theme_fonts');
 
 
 
-// Panel and Section
-// Global Panel
+// All Color
 function sample_theme_customize($wp_customize){
+    $wp_customize-> add_panel('sample_theme_allcolor', array(
+        'title' => __('All Color', 'sample_theme'),
+        'priority' => 20,
+    ));
+
+
+    // Background Color Section
+    $wp_customize-> add_section('sample_theme_color', array(
+        'title' => __('Background Color', 'sample_theme'),
+        'description' => __('If you interested to update your background color, you can do it here.', 'sample_theme'),
+        'priority' => 10,
+        'panel' => 'sample_theme_allcolor',
+    ));
+    $wp_customize->add_setting('sample_theme_color_picker', array(
+        'default' => '#ffffff',
+    ));
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'sample_theme_color_picker', array(
+        'label' => __('Color Picker', 'sample_theme'),
+        'description' => __('If you interested to update your background color, you can do it here.', 'sample_theme'),
+        'setting' => 'sample_theme_color_picker',
+        'section' => 'sample_theme_color',
+    )));
+
+    // Header Color Section
+    $wp_customize-> add_section('sample_header_color', array(
+        'title' => __('Header Background Color', 'sample_theme'),
+        'description' => __('If you interested to update your headervbg color, you can do it here.', 'sample_theme'),
+        'priority' => 20,
+        'panel' => 'sample_theme_allcolor',
+    ));
+    $wp_customize->add_setting('sample_headercolor', array(
+        'default' => '#ffffff',
+    ));
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'sample_headercolor', array(
+        'label' => __('Color Picker', 'sample_theme'),
+        'description' => __('If you interested to update your header bg color, you can do it here.', 'sample_theme'),
+        'setting' => 'sample_headercolor',
+        'section' => 'sample_header_color',
+    )));
+
+    // Coppyright color Section
+    $wp_customize-> add_section('sample_coppy_color', array(
+        'title' => __('Coppyright Background Color', 'sample_theme'),
+        'description' => __('If you interested to update your coppyright bg color, you can do it here.', 'sample_theme'),
+        'priority' => 30,
+        'panel' => 'sample_theme_allcolor',
+    ));
+    $wp_customize->add_setting('sample_coppyright', array(
+        'default' => '#000',
+    ));
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'sample_coppyright', array(
+        'label' => __('Color Picker', 'sample_theme'),
+        'description' => __('If you interested to update your coppyright bg color, you can do it here.', 'sample_theme'),
+        'setting' => 'sample_coppyright',
+        'section' => 'sample_coppy_color',
+    )));
+
+
+
+
+
+
+}
+add_action('customize_register', 'sample_theme_customize');
+
+
+
+// Customizer CSS
+function sample_theme_customizer_css(){
+    ?>
+    <style type="text/css">
+        body{
+            background-color: <?php echo get_theme_mod('sample_theme_color_picker', '#ffffff'); ?>;
+        }
+        #header_area{
+            background-color: <?php echo get_theme_mod('sample_headercolor', '#ffffff'); ?>;
+        }
+        .coppyright_area{
+            background-color: <?php echo get_theme_mod('sample_coppyright', '#000'); ?>;
+        }
+
+        
+    </style>
+
+    <?php
+}
+add_action('wp_head', 'sample_theme_customizer_css');
+
+
+
+
+function sample_global_customize($wp_customize){
     $wp_customize-> add_panel('sample_theme_global', array(
         'title' => __('Global', 'sample_theme'),
         'priority' => 10,
     ));
 
-    // Logo Section
-    $wp_customize-> add_section('sample_theme_logo', array(
-        'title' =>__('Logo', 'sample_theme'),
+
+      // Logo Section
+      $wp_customize-> add_section('sample_theme_logo', array(
+        'title' =>__('Logo Upload', 'sample_theme'),
         'description' => __('Upload your logo here', 'sample_theme'),
         'priority' => 10,
         'panel' => 'sample_theme_global',
@@ -56,46 +148,12 @@ function sample_theme_customize($wp_customize){
     )));
 
 
-    // Logo Name Color Section
-    $wp_customize-> add_section('sample_theme_color', array(
-        'title' => __('Background Color', 'sample_theme'),
-        'description' => __('If you interested to update your Background color, you can do it here.', 'sample_theme'),
-        'priority' => 20,
-        'panel' => 'sample_theme_global',
-    ));
-    $wp_customize->add_setting('sample_theme_color_picker', array(
-        'default' => '#000',
-    ));
-    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'sample_theme_color_picker', array(
-        'label' => __('Color Picker', 'sample_theme'),
-        'description' => __('If you interested to update your Background color, you can do it here.', 'sample_theme'),
-        'setting' => 'sample_theme_color_picker',
-        'section' => 'sample_theme_color',
-    )));
-
 
 
 
 
 
 }
-add_action('customize_register', 'sample_theme_customize');
+add_action('customize_register', 'sample_global_customize');
 
 
-
-
-
-// Customizer CSS
-function sample_theme_customizer_css(){
-    ?>
-    <style type="text/css">
-        body{
-            background-color: <?php echo get_theme_mod('sample_theme_color_picker', '#ffffff'); ?>;
-        }
-
-        
-    </style>
-
-    <?php
-}
-add_action('wp_head', 'sample_theme_customizer_css');
