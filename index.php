9f2851ec36f6b340c0331054ee26d400f28f19e6
MD. Shohel Rana <shohel065931@gmail.com>
2025-05-05 20:21:52 +0600

    f

--- index.php
+++ index.php
@@ -8,6 +8,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <?php
+                if ( have_posts()) :
+                    while (have_posts()) : the_post(); 
+               ?>
+                <div class="blog_area">
+
+                </div>
+                <?php  
+                 endwhile ;
+                 else 
+                    _e('No Post Found');
+                 endif ;
+                 
+                 ?>
+
+
+
                <h2><?php  the_title(); ?></h2> 
                <p><?php  the_content(); ?></p> 
             </div>
